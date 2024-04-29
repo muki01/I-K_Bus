b251bc0099fcace0a6387b5178d2bac20b8f94fc
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2024-04-29 22:41:47 +0300

    Some Changes

--- E46_KBus_Code/E46_KBus_Code.ino
+++ E46_KBus_Code/E46_KBus_Code.ino
@@ -7,8 +7,10 @@ IbusSerial ibus;
 byte source, length, destination, databytes[36];
 
 static bool carLocked = false;
+static bool goodByeLightsOn = false;
 int top_Btn_Presed = 0;
 int mid_Btn_Presed = 0;
+static unsigned long goodByeLightsTime = 0;
 static unsigned long lastTimeTopBtn = 0;
 static unsigned long lastTimeMidBtn = 0;
 
@@ -23,6 +25,12 @@ void setup() {
 }
 void loop() {
   ibus.run();
+
+  if (goodByeLightsOn == true && millis() - goodByeLightsTime > 2000) {
+    goodByeLightsOn = false;
+    goodByeLightsTime = millis();
+    ibus.write(TurnOffLights, sizeof(TurnOffLights));
+  }
 }
 
 void packetHandler(byte *packet) {
@@ -35,38 +43,41 @@ void packetHandler(byte *packet) {
 
   if ((source == 0x00) && (destination == 0xBF) && (databytes[0] == 0x72) && (databytes[1] == 0x22)) {
     carLocked = false;
-    unsigned long currentTime = millis();
-    unsigned long timeSinceLastEntry = currentTime - lastTimeTopBtn;
 
-    if (timeSinceLastEntry >= 14500) {
+    if (millis() - lastTimeTopBtn >= 14500) {
       top_Btn_Presed = 0;
     }
+
     if (top_Btn_Presed == 0) {
       ibus.write(ParkLights_And_Signals, sizeof(ParkLights_And_Signals));
       top_Btn_Presed = 1;
-      lastTimeTopBtn = currentTime;
+      lastTimeTopBtn = millis();
     } else {
       ibus.write(ParkLights_And_Signals_And_FogLights, sizeof(ParkLights_And_Signals_And_FogLights));
       top_Btn_Presed = 0;
-      lastTimeTopBtn = currentTime;
+      lastTimeTopBtn = millis();
     }
   }
 
   if ((source == 0x00) && (destination == 0xBF) && (databytes[0] == 0x72) && (databytes[1] == 0x12)) {
     top_Btn_Presed = 0;
+
     if (carLocked == false) {
+      goodByeLightsOn = true;
+      goodByeLightsTime = millis();
       carLocked = true;
+      ibus.write(GoodbyeLights, sizeof(GoodbyeLights));
     } else if (carLocked) {
       mid_Btn_Presed++;
-      unsigned long currentTime = millis();
-      unsigned long timeSinceLastEntry = currentTime - lastTimeMidBtn;
-      if (mid_Btn_Presed == 2 && timeSinceLastEntry <= 4000) {
+
+      if (mid_Btn_Presed == 2 && millis() - lastTimeMidBtn <= 4000) {
         ibus.write(FollowMeHome, sizeof(FollowMeHome));
         mid_Btn_Presed = 0;
-      } else if (timeSinceLastEntry > 4000) {
+      } else if (millis() - lastTimeMidBtn > 4000) {
         mid_Btn_Presed = 1;
       }
-      lastTimeMidBtn = currentTime;
+
+      lastTimeMidBtn = millis();
     }
   }
 }
