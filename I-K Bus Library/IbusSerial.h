6ec80a440c4a353fb1d565abcc1292b594be67cf
Muksin Muksin <75759731+muki01@users.noreply.github.com>
2025-08-26 14:25:04 +0300

    Refactor IbusSerial.h for code clarity

--- I-K Bus Library/IbusSerial.h
+++ I-K Bus Library/IbusSerial.h
@@ -7,51 +7,48 @@
 
 #define IBUS_DEBUG
 
-class IbusSerial
-{
-
-public:
-	IbusSerial();
-	~IbusSerial();
-	void setIbusSerial(HardwareSerial &newIbusSerial);
+class IbusSerial {
+ public:
+  IbusSerial();
+  ~IbusSerial();
+  void setIbusSerial(HardwareSerial &newIbusSerial);
 #if defined(IBUS_DEBUG)
-	void setIbusDebug(Stream &newIbusDebug);
+  void setIbusDebug(Stream &newIbusDebug);
 #endif
-	void run();
-	void write(const byte message[], byte size);
-	void printDebugMessage(const char *debugPrefix);
-	typedef void IbusPacketHandler_t(byte *packet);
-	void setIbusPacketHandler(IbusPacketHandler_t newHandler);
-	void contentionTimer();
-	void startTimer();
-	void sleepEnable(unsigned long sleepTime);
-	void sleep();
+  void run();
+  void write(const byte message[], byte size);
+  void printDebugMessage(const char *debugPrefix);
+  typedef void IbusPacketHandler_t(byte *packet);
+  void setIbusPacketHandler(IbusPacketHandler_t newHandler);
+  void contentionTimer();
+  void startTimer();
+  void sleepEnable(unsigned long sleepTime);
+  void sleep();
 
-private:
-	enum ibusFsmStates
-	{
-		FIND_SOURCE,   // Read source byte
-		FIND_LENGTH,   // Read length byte
-		FIND_MESSAGE,  // Read in main body of message
-		GOOD_CHECKSUM, // Process message if checksum good
-		BAD_CHECKSUM,  // Debug print bad message if checksum bad
-	} ibusState;
+ private:
+  enum ibusFsmStates {
+    FIND_SOURCE,    // Read source byte
+    FIND_LENGTH,    // Read length byte
+    FIND_MESSAGE,   // Read in main body of message
+    GOOD_CHECKSUM,  // Process message if checksum good
+    BAD_CHECKSUM,   // Debug print bad message if checksum bad
+  } ibusState;
 
-	void readIbus();
-	void compareIbusPacket();
-	void sendIbusPacket();
-	void sendIbusMessageIfAvailable();
-	HardwareSerial *IbusSerial;
+  void readIbus();
+  void compareIbusPacket();
+  void sendIbusPacket();
+  void sendIbusMessageIfAvailable();
+  HardwareSerial *IbusSerial;
 #if defined(IBUS_DEBUG)
-	Stream *IbusDebug;
+  Stream *IbusDebug;
 #endif
-	byte source;
-	byte length;
-	byte destination;
-	byte ibusByte[40];
-	boolean busInSync;
-	static const byte packetGap = 10;
-	byte *pData;
-	IbusPacketHandler_t *pIbusPacketHandler;
+  byte source;
+  byte length;
+  byte destination;
+  byte ibusByte[40];
+  boolean busInSync;
+  static const byte packetGap = 10;
+  byte *pData;
+  IbusPacketHandler_t *pIbusPacketHandler;
 };
 #endif
